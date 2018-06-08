7cbd5a36ce892ce25d6f2d02dcacdd8cae0ed29c
Sujan Kandeepan <Sujan.Kandeepan@cpii.com>
2018-06-08 13:26:05 -0400

    Changes and fixes to Excel file

--- SatcomRfWebsite/Models/TestData.cs
+++ SatcomRfWebsite/Models/TestData.cs
@@ -12,15 +12,14 @@ namespace SatcomRfWebsite.Models
         public string MaxResult { get; set; }
         public string AvgResult { get; set; }
         public string StdDev { get; set; }
-        public string Cpk { get; set; }
         public string Unit { get; set; }
         public List<List<string>> AllResultsConv { get; set; }
         public string MinResultConv { get; set; }
         public string MaxResultConv { get; set; }
         public string AvgResultConv { get; set; }
         public string StdDevConv { get; set; }
-        public string CpkConv { get; set; }
         public string UnitConv { get; set; }
+        public string Cpk { get; set; }
 
         public TestData() { }
     }
