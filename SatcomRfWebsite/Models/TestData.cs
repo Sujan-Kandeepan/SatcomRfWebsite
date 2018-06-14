a54923e1475d6c028a866b89f07fabe6ac967fbb
Sujan Kandeepan <sujank@rogers.com>
2018-06-14 12:52:02 -0400

    Removed debug log

--- SatcomRfWebsite/Models/TestData.cs
+++ SatcomRfWebsite/Models/TestData.cs
@@ -8,7 +8,7 @@ namespace SatcomRfWebsite.Models
         public string TestName { get; set; }
         public string Channel { get; set; }
         public string Power { get; set; }
-        public List<List<string>> AllResults { get; set; } // SN, Result, StartTime, LowLimit, UpLimit
+        public List<ResultData> AllResults { get; set; }
         public string StartTimePlaceHolder { get; set; }
         public string ResultsPlaceHolder { get; set; }
         public string MinResult { get; set; }
@@ -16,7 +16,7 @@ namespace SatcomRfWebsite.Models
         public string AvgResult { get; set; }
         public string StdDev { get; set; }
         public string Unit { get; set; }
-        public List<List<string>> AllResultsConv { get; set; } // SN, ResultConv
+        public string ResultConvPlaceholder { get; set; }
         public string MinResultConv { get; set; }
         public string MaxResultConv { get; set; }
         public string AvgResultConv { get; set; }
@@ -35,15 +35,35 @@ namespace SatcomRfWebsite.Models
         public string Channel { get; set; }
         public string Power { get; set; }
         public string Units { get; set; }
-        public List<List<string>> Results { get; set; }
+        public List<ResultData> Results { get; set; }
 
-        public TestInfo(string inTestName, string inChannel, string inPower, string inUnits, List<List<string>> inResults)
+        public TestInfo(string inTestName, string inChannel, string inPower, string inUnits, List<ResultData> inResults)
         {
             TestName = inTestName;
             Channel = inChannel;
             Power = inPower;
             Units = inUnits;
             Results = inResults;
+        }
+    }
+
+    public class ResultData
+    {
+        public string SerialNumber { get; set; }
+        public string StartTime { get; set; }
+        public string Result { get; set; }
+        public string ResultConv { get; set; }
+        public string LowLimit { get; set; }
+        public string UpLimit { get; set; }
+
+        public ResultData(string inSerialNumber, string inStartTime, string inResult, string inResultConv, string inLowLimit, string inUpLimit)
+        {
+            SerialNumber = inSerialNumber;
+            StartTime = inStartTime;
+            Result = inResult;
+            ResultConv = inResultConv;
+            LowLimit = inLowLimit;
+            UpLimit = inUpLimit;
         }
     }
 }
