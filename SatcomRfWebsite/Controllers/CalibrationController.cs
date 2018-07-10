6599808b9299197cbb5291eb2027f40e9e9be6ec
Sujan Kandeepan <Sujan.Kandeepan@cpii.com>
2018-07-10 16:16:06 -0400

    Added empty controller and view for calibration page

---
+++ SatcomRfWebsite/Controllers/CalibrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SatcomRfWebsite
+{
+    public class CalibrationController : Controller
+    {
+        // GET: Calibration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
