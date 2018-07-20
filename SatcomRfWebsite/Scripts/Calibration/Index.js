336b1e00eab199206dadfb9a4a17c1625cdece37
Sujan Kandeepan <Sujan.Kandeepan@cpii.com>
2018-07-20 11:10:39 -0400

    Removed redundant script, moved code into razor file

--- SatcomRfWebsite/Scripts/Calibration/Index.js
+++
@@ -1,7 +0,0 @@
-﻿function setURL(value) {
-    if (value == "default") {
-        location.href = location.origin + "/Calibration/Create";
-    } else {
-        location.href = location.origin + "/Calibration/Create/" + value.replace(" ", "");
-    }
-}
